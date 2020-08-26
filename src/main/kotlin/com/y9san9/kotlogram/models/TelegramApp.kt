c7b3aee1dae29aa541b02b9d6e920054e9e95b42
kylichist <pimenovpetya@gmail.com>
2020-08-26 14:17:56 +0300

    #3

--- src/main/kotlin/com/y9san9/kotlogram/models/TelegramApp.kt
+++ src/main/kotlin/com/y9san9/kotlogram/models/TelegramApp.kt
@@ -7,10 +7,10 @@ import com.github.badoualy.telegram.api.TelegramApp
 data class TelegramApp(
         val apiId: Int,
         val apiHash: String,
-        val deviceModel: String,
-        val systemVersion: String,
-        val appVersion: String,
-        val langCode: String
+        val deviceModel: String = "Web",
+        val systemVersion: String = "Kotlogram2",
+        val appVersion: String = "Api",
+        val langCode: String = "en"
 ) {
     internal fun toKotlogramApp() = TelegramApp(apiId, apiHash, deviceModel, systemVersion, appVersion, langCode)
 }
